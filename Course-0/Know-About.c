53f0d0da83b55fc57dd28c21ae150e2e3655a507
Soru <ityouna007@gmail.com>
2023-09-23 04:04:59 +0900

    ✨ NOTについて学んだ

---
+++ Course-0/Know-About.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+//     double x = 1.0;  // 初期の点のx座標
+//     double y = 0.0;  // 初期の点のy座標
+//     double z = 0.0;  // 初期の点のz座標 (この例では変動しない)
+//     double theta = M_PI / 4;  // 45度をラジアンに変換
+
+//     // Z軸を中心とした回転行列を使用して新しい座標を計算
+//     double newX = x * cos(theta) - y * sin(theta);
+//     double newY = x * sin(theta) + y * cos(theta);
+
+//     printf("初期座標: (%f, %f, %f)\n", x, y, z);
+//     printf("回転後の座標: (%f, %f, %f)\n", newX, newY, z);
+int rock = 0;
+if (!rock){
+    printf("not go");
+}else{
+    printf("go");
+}
+   return 0;
+ }
+
